b9f01a2f6fc849966223ee1e3c90bdd0c7ab03f6
VovaHayek <vova.hayek@gmail.com>
2023-04-18 00:40:00 +0300

    Creating removing files/dirs in replica, that are not in source folder

--- syncronizer.py
+++ syncronizer.py
@@ -1,4 +1,5 @@
 import hashlib
+import shutil
 import time
 import os
 
@@ -9,6 +10,7 @@ class SyncronizeFolders:
         self.replica = destination_folder
 
     def check_files(self, source_file, replica_file):
+        """Checking if content of both files is the same or not"""
         if os.path.isfile(source_file):
             with open(source_file, 'rb') as source_f:
                 with open(replica_file, 'rb') as replica_f:
@@ -18,6 +20,7 @@ class SyncronizeFolders:
         return True
     
     def get_all_files(self):
+        """Getting all the files/dirs from source and replica folders and appending them to a dictionaries"""
         source_files = {}
         source_dirs = {}
 
@@ -41,26 +44,27 @@ class SyncronizeFolders:
         return source_files, source_dirs, replica_files, replica_dirs
 
     def check_main_folders(self):
+        """Checking if files and dirs are the same in replica folder and in source folder"""
         source_files, source_dirs, replica_files, replica_dirs = self.get_all_files()
+
+        if len(source_files) != len(replica_files) or len(source_dirs) != len(replica_dirs):
+            return False
 
         for source_dir in source_dirs:
             if not source_dir in replica_dirs:
                 return False
 
-
         for source_file, source_root in source_files.items():
             if source_file in replica_files:
                 if not self.check_files(source_root, replica_files[source_file]):
-                    print('52')
                     return False
             else:
-                print('56')
                 return False
         else:
             return True
 
-    #Function for removing/updating/adding files if replica folder doesn't match source folder
     def syncronize(self):
+        """Updating replica folder if files/dirs are not the same in both folders"""
         source_files, source_dirs, replica_files, replica_dirs = self.get_all_files()
 
         for source_dir_name, source_dir in source_dirs.items():
@@ -80,12 +84,20 @@ class SyncronizeFolders:
                     os.remove(replica_files[source_filename])
                     os.system(f'xcopy \"{source_file}\" \"{self.replica}{destination_file[1]}\" /I /F')
                     print(f'Changed content of - {source_filename}')
+        
+        for replica_dir_name, replica_dir in replica_dirs.items():
+            if not replica_dir_name in source_dirs:
+                shutil.rmtree(replica_dir)
+                return self.syncronize()
+        
+        for replica_filename, replica_file in replica_files.items():
+            if not replica_filename in source_files:
+                os.remove(replica_file)
 
     #Main loop
     def main(self):
         while True:
             if self.check_main_folders():
-                print("Everything is fine")
                 time.sleep(5)
                 continue
             else:
